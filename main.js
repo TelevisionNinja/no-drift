e2f85cf702cd5ae9e730f5b7c7b2ee475a6ffeed
TelevisionNinja <TelevisionNinja@gmail.com>
2022-02-25 00:14:06 -0600

    moved exports to folder and added node version

--- main.js
+++
@@ -1,3 +0,0 @@
-export * from './src/nodrift.js';
-export * from './src/nodriftzero.js';
-export * from './src/worker/nodriftworker.js';
---
+++ src/exports.js
@@ -0,0 +1,3 @@
+export * from './nodrift.js';
+export * from './nodriftzero.js';
+export * from './worker/nodriftworker.js';
